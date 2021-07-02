508a2064c8fdda36bad5a2cb861b30bd07c1bfdb
siddh <siddh@DESKTOP-N2FRK8U>
2021-07-02 11:54:58 +0530

    Admin File commited

--- Stars-for-Participants/src/main/java/com/starts/Model/Admin.java
+++ Stars-for-Participants/src/main/java/com/starts/Model/Admin.java
@@ -1,5 +1,29 @@
 package com.starts.Model;
 
 public class Admin {
-
+	
+	private int userId;
+	private String password;
+	public Admin(int userId, String password) {
+		super();
+		this.userId = userId;
+		this.password = password;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [userId=" + userId + ", password=" + password + "]";
+	}
+	
 }
