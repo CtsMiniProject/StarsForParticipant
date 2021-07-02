3bbcb53d605cbae7401165c607c11e99958f46e8
Sharique Anwar <anwarsharique597@gmail.com>
2021-07-02 12:09:37 +0530

    pojo class admin updated

--- Stars-for-Participants/src/main/java/com/starts/Model/Admin.java
+++ Stars-for-Participants/src/main/java/com/starts/Model/Admin.java
@@ -1,5 +1,30 @@
 package com.starts.Model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.Pattern;
+
+@Entity
 public class Admin {
+	@Id
+	private int adminId;
+	@Pattern(regexp = "^(?=.{6,}$)(?=.\\W).$", message = "Should have atleast 1 special character and be minimum of 6 characters")
+	private String password;
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [adminId=" + adminId + ", password=" + password + "]";
+	}
 
 }
